73d6987a4b142a8f9f655317594a658834fe9a3b
Rassouan Richardsons <R.Richardsons@hotmail.fr>
2019-09-20 08:16:18 +0200

    mise a jour 20/09 08:16
    mise a jour 20/09 08:16

--- veloc.sql
+++ veloc.sql
@@ -1,22 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 19 sep. 2019 à 14:28
+-- Hôte : localhost:8889
+-- Généré le :  ven. 20 sep. 2019 à 06:13
 -- Version du serveur :  5.7.26
--- Version de PHP :  7.2.18
+-- Version de PHP :  7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `veloc`
@@ -28,16 +20,13 @@ SET time_zone = "+00:00";
 -- Structure de la table `adresses_utilisateurs`
 --
 
-DROP TABLE IF EXISTS `adresses_utilisateurs`;
-CREATE TABLE IF NOT EXISTS `adresses_utilisateurs` (
-  `id_adresses_utilisateur` int(11) NOT NULL AUTO_INCREMENT,
-  `numero` int(11) DEFAULT NULL,
-  `rue` varchar(70) NOT NULL,
-  `extension` varchar(45) DEFAULT NULL,
-  `villes_utilisateur_id_ville` int(11) NOT NULL,
-  PRIMARY KEY (`id_adresses_utilisateur`),
-  UNIQUE KEY `id_adresses_utilisateurs_UNIQUE` (`id_adresses_utilisateur`),
-  KEY `fk_adresses_utilisateurs_villes_utilisateur1_idx` (`villes_utilisateur_id_ville`)
+CREATE TABLE `adresses_utilisateurs` (
+  `id_adresses_utilisateur` int(11) NOT NULL,
+  `villes_habitations` varchar(255) NOT NULL,
+  `numero_habitations` int(11) DEFAULT NULL,
+  `rue_habitations` varchar(70) NOT NULL,
+  `extension_habitations` varchar(45) DEFAULT NULL,
+  `villes_utilisateur_id_ville` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -46,16 +35,14 @@ CREATE TABLE IF NOT EXISTS `adresses_utilisateurs` (
 -- Structure de la table `articles`
 --
 
-DROP TABLE IF EXISTS `articles`;
-CREATE TABLE IF NOT EXISTS `articles` (
-  `idarticles` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `articles` (
+  `idarticles` int(11) NOT NULL,
   `nom_article` varchar(45) NOT NULL,
   `description_article` varchar(45) NOT NULL,
   `prix_article_HT` int(11) NOT NULL,
   `taux_tva` int(11) NOT NULL,
-  `type_article` tinyint(1) NOT NULL COMMENT '0 = meca; 1=elec',
-  PRIMARY KEY (`idarticles`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+  `type_article` tinyint(1) NOT NULL COMMENT '0 = meca; 1=elec'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `articles`
@@ -70,14 +57,10 @@ INSERT INTO `articles` (`idarticles`, `nom_article`, `description_article`, `pri
 -- Structure de la table `bornes_ville`
 --
 
-DROP TABLE IF EXISTS `bornes_ville`;
-CREATE TABLE IF NOT EXISTS `bornes_ville` (
-  `id_bornes_ville` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `bornes_ville` (
+  `id_bornes_ville` int(11) NOT NULL,
   `borne_adresse` varchar(45) NOT NULL,
-  `adresses_utilisateurs_id_adresses_utilisateur` int(11) NOT NULL,
-  PRIMARY KEY (`id_bornes_ville`),
-  UNIQUE KEY `id_bornes_ville_UNIQUE` (`id_bornes_ville`),
-  KEY `fk_bornes_ville_adresses_utilisateurs1_idx` (`adresses_utilisateurs_id_adresses_utilisateur`)
+  `adresses_utilisateurs_id_adresses_utilisateur` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -86,14 +69,12 @@ CREATE TABLE IF NOT EXISTS `bornes_ville` (
 -- Structure de la table `cb`
 --
 
-DROP TABLE IF EXISTS `cb`;
-CREATE TABLE IF NOT EXISTS `cb` (
+CREATE TABLE `cb` (
   `idCB` int(11) NOT NULL,
   `CB_number` varchar(45) NOT NULL,
   `CB_name` varchar(45) NOT NULL,
   `CB_validdate` datetime NOT NULL,
-  `CB_activite` tinyint(4) NOT NULL,
-  PRIMARY KEY (`idCB`)
+  `CB_activite` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -102,17 +83,14 @@ CREATE TABLE IF NOT EXISTS `cb` (
 -- Structure de la table `entreprises`
 --
 
-DROP TABLE IF EXISTS `entreprises`;
-CREATE TABLE IF NOT EXISTS `entreprises` (
+CREATE TABLE `entreprises` (
   `identreprise` int(11) NOT NULL,
   `nom_entreprise` varchar(45) NOT NULL,
   `numeroRCS` varchar(45) NOT NULL,
   `adresse_entreprise` varchar(80) DEFAULT NULL,
   `ville_entreprise` varchar(45) DEFAULT NULL,
   `cp_entreprise` int(5) DEFAULT NULL,
-  `types_utilisateur_iduser_type` int(11) NOT NULL,
-  PRIMARY KEY (`identreprise`),
-  KEY `fk_entreprises_types_utilisateur1_idx` (`types_utilisateur_iduser_type`)
+  `types_utilisateur_iduser_type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -121,18 +99,13 @@ CREATE TABLE IF NOT EXISTS `entreprises` (
 -- Structure de la table `paniers`
 --
 
-DROP TABLE IF EXISTS `paniers`;
-CREATE TABLE IF NOT EXISTS `paniers` (
-  `id_paniers` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `paniers` (
+  `id_paniers` int(11) NOT NULL,
   `prix_total` int(11) DEFAULT NULL,
   `panier_activite` tinyint(4) DEFAULT NULL,
   `panier_litige` varchar(255) DEFAULT NULL,
   `utilisateurs_id_utilisateur` int(11) NOT NULL,
-  `articles_idarticles` int(11) NOT NULL,
-  PRIMARY KEY (`id_paniers`),
-  UNIQUE KEY `id_paniers_UNIQUE` (`id_paniers`),
-  KEY `fk_paniers_utilisateurs1_idx` (`utilisateurs_id_utilisateur`),
-  KEY `fk_paniers_articles1_idx` (`articles_idarticles`)
+  `articles_idarticles` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -141,14 +114,11 @@ CREATE TABLE IF NOT EXISTS `paniers` (
 -- Structure de la table `sexe`
 --
 
-DROP TABLE IF EXISTS `sexe`;
-CREATE TABLE IF NOT EXISTS `sexe` (
-  `id_sexe` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `sexe` (
+  `id_sexe` int(11) NOT NULL,
   `sexecourt` varchar(1) NOT NULL COMMENT 'm ou h',
-  `sexelong` varchar(8) NOT NULL COMMENT 'masculin ou feminin',
-  PRIMARY KEY (`id_sexe`),
-  UNIQUE KEY `id_sexe_UNIQUE` (`id_sexe`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `sexelong` varchar(8) NOT NULL COMMENT 'masculin ou feminin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `sexe`
@@ -164,19 +134,15 @@ INSERT INTO `sexe` (`id_sexe`, `sexecourt`, `sexelong`) VALUES
 -- Structure de la table `stock`
 --
 
-DROP TABLE IF EXISTS `stock`;
-CREATE TABLE IF NOT EXISTS `stock` (
-  `id_stock` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `stock` (
+  `id_stock` int(11) NOT NULL,
   `date_maj` datetime NOT NULL,
   `nb_velo_elec` int(11) NOT NULL,
   `nb_velo_meca` int(11) NOT NULL,
   `nb_meca_loues` int(11) NOT NULL,
   `nb_elec_loues` int(11) NOT NULL,
   `nb_meca_HS` int(11) NOT NULL,
-  `nb_elec_HS` int(11) NOT NULL,
-  PRIMARY KEY (`id_stock`),
-  UNIQUE KEY `idvelos_UNIQUE` (`id_stock`),
-  KEY `fk_stock_articles1_idx` (`nb_meca_loues`)
+  `nb_elec_HS` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -185,15 +151,11 @@ CREATE TABLE IF NOT EXISTS `stock` (
 -- Structure de la table `tarifs`
 --
 
-DROP TABLE IF EXISTS `tarifs`;
-CREATE TABLE IF NOT EXISTS `tarifs` (
+CREATE TABLE `tarifs` (
   `idtarif` int(11) NOT NULL,
   `tarifscol` varchar(45) DEFAULT NULL,
   `articles_idarticles` int(11) NOT NULL,
-  `time_idtime` int(11) NOT NULL,
-  PRIMARY KEY (`idtarif`),
-  KEY `fk_tarifs_articles1_idx` (`articles_idarticles`),
-  KEY `fk_tarifs_time1_idx` (`time_idtime`)
+  `time_idtime` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -202,12 +164,10 @@ CREATE TABLE IF NOT EXISTS `tarifs` (
 -- Structure de la table `time`
 --
 
-DROP TABLE IF EXISTS `time`;
-CREATE TABLE IF NOT EXISTS `time` (
+CREATE TABLE `time` (
   `idtime` int(11) NOT NULL,
   `HJSM` varchar(45) NOT NULL,
-  `timecol` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idtime`)
+  `timecol` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -216,12 +176,9 @@ CREATE TABLE IF NOT EXISTS `time` (
 -- Structure de la table `types_utilisateur`
 --
 
-DROP TABLE IF EXISTS `types_utilisateur`;
-CREATE TABLE IF NOT EXISTS `types_utilisateur` (
-  `iduser_type` int(11) NOT NULL AUTO_INCREMENT,
-  `membre_entreprise` tinyint(4) NOT NULL,
-  PRIMARY KEY (`iduser_type`),
-  UNIQUE KEY `iduser_type_UNIQUE` (`iduser_type`)
+CREATE TABLE `types_utilisateur` (
+  `iduser_type` int(11) NOT NULL,
+  `membre_entreprise` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -230,25 +187,28 @@ CREATE TABLE IF NOT EXISTS `types_utilisateur` (
 -- Structure de la table `utilisateurs`
 --
 
-DROP TABLE IF EXISTS `utilisateurs`;
-CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT,
-  `nom_utlilisateur` varchar(45) NOT NULL,
-  `prenom_utlilisateur` varchar(45) NOT NULL,
-  `date_naissance_utlilisateur` datetime NOT NULL,
-  `adresse_mail_utlilisateur` varchar(255) NOT NULL,
-  `password_utlilisateur` varchar(45) NOT NULL,
-  `adresses_utilisateurs_id_adresses_utilisateur` int(11) NOT NULL,
-  `sexe_id_sexe` int(11) NOT NULL,
-  `types_utilisateur_iduser_type` int(11) NOT NULL,
-  `CB_idCB` int(11) NOT NULL,
-  PRIMARY KEY (`id_utilisateur`),
-  UNIQUE KEY `idusers_UNIQUE` (`id_utilisateur`),
-  KEY `fk_utilisateurs_adresses_utilisateurs_idx` (`adresses_utilisateurs_id_adresses_utilisateur`),
-  KEY `fk_utilisateurs_sexe1_idx` (`sexe_id_sexe`),
-  KEY `fk_utilisateurs_types_utilisateur1_idx` (`types_utilisateur_iduser_type`),
-  KEY `fk_utilisateurs_CB1_idx` (`CB_idCB`)
+CREATE TABLE `utilisateurs` (
+  `id_utilisateur` int(11) NOT NULL,
+  `sexe_utilisateur` varchar(1) NOT NULL COMMENT 'm=Male f=Femelle',
+  `nom_utilisateur` varchar(45) NOT NULL,
+  `prenom_utilisateur` varchar(45) NOT NULL,
+  `date_naissance_utilisateur` date NOT NULL,
+  `adresse_mail_utilisateur` varchar(255) NOT NULL,
+  `password_utilisateur` varchar(255) NOT NULL,
+  `date_creation_utilisateurs` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `adresses_utilisateurs_id_adresses_utilisateur` int(11) DEFAULT NULL,
+  `sexe_id_sexe` int(11) DEFAULT NULL COMMENT 'A SUPPRIMER PUTAIN',
+  `types_utilisateur_iduser_type` int(11) DEFAULT NULL,
+  `CB_idCB` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id_utilisateur`, `sexe_utilisateur`, `nom_utilisateur`, `prenom_utilisateur`, `date_naissance_utilisateur`, `adresse_mail_utilisateur`, `password_utilisateur`, `date_creation_utilisateurs`, `adresses_utilisateurs_id_adresses_utilisateur`, `sexe_id_sexe`, `types_utilisateur_iduser_type`, `CB_idCB`) VALUES
+(1, 'm', 'x', 'x', '1994-10-01', 'x@x.fr', '2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881', '2019-09-19 16:00:41', NULL, NULL, NULL, NULL),
+(2, 'f', 'Ginette', 'Ginette', '1943-10-01', 'Ginette@GrosseMoula.fr', 'cd2eb0837c9b4c962c22d2ff8b5441b7b45805887f051d39bf133b583baf6860', '2019-09-19 17:27:13', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -256,14 +216,11 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 -- Structure de la table `villes_utilisateur`
 --
 
-DROP TABLE IF EXISTS `villes_utilisateur`;
-CREATE TABLE IF NOT EXISTS `villes_utilisateur` (
-  `id_ville` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `villes_utilisateur` (
+  `id_ville` int(11) NOT NULL,
   `nom_ville` varchar(45) NOT NULL,
-  `ville_activite` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`id_ville`),
-  UNIQUE KEY `idville_UNIQUE` (`id_ville`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `ville_activite` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `villes_utilisateur`
@@ -274,6 +231,165 @@ INSERT INTO `villes_utilisateur` (`id_ville`, `nom_ville`, `ville_activite`) VAL
 (2, 'Lille', 1),
 (3, 'Lyon', 1),
 (4, 'Bordeaux', 1);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `adresses_utilisateurs`
+--
+ALTER TABLE `adresses_utilisateurs`
+  ADD PRIMARY KEY (`id_adresses_utilisateur`),
+  ADD UNIQUE KEY `id_adresses_utilisateurs_UNIQUE` (`id_adresses_utilisateur`),
+  ADD KEY `fk_adresses_utilisateurs_villes_utilisateur1_idx` (`villes_utilisateur_id_ville`);
+
+--
+-- Index pour la table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`idarticles`);
+
+--
+-- Index pour la table `bornes_ville`
+--
+ALTER TABLE `bornes_ville`
+  ADD PRIMARY KEY (`id_bornes_ville`),
+  ADD UNIQUE KEY `id_bornes_ville_UNIQUE` (`id_bornes_ville`),
+  ADD KEY `fk_bornes_ville_adresses_utilisateurs1_idx` (`adresses_utilisateurs_id_adresses_utilisateur`);
+
+--
+-- Index pour la table `cb`
+--
+ALTER TABLE `cb`
+  ADD PRIMARY KEY (`idCB`);
+
+--
+-- Index pour la table `entreprises`
+--
+ALTER TABLE `entreprises`
+  ADD PRIMARY KEY (`identreprise`),
+  ADD KEY `fk_entreprises_types_utilisateur1_idx` (`types_utilisateur_iduser_type`);
+
+--
+-- Index pour la table `paniers`
+--
+ALTER TABLE `paniers`
+  ADD PRIMARY KEY (`id_paniers`),
+  ADD UNIQUE KEY `id_paniers_UNIQUE` (`id_paniers`),
+  ADD KEY `fk_paniers_utilisateurs1_idx` (`utilisateurs_id_utilisateur`),
+  ADD KEY `fk_paniers_articles1_idx` (`articles_idarticles`);
+
+--
+-- Index pour la table `sexe`
+--
+ALTER TABLE `sexe`
+  ADD UNIQUE KEY `id_sexe` (`id_sexe`);
+
+--
+-- Index pour la table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`id_stock`),
+  ADD UNIQUE KEY `idvelos_UNIQUE` (`id_stock`),
+  ADD KEY `fk_stock_articles1_idx` (`nb_meca_loues`);
+
+--
+-- Index pour la table `tarifs`
+--
+ALTER TABLE `tarifs`
+  ADD PRIMARY KEY (`idtarif`),
+  ADD KEY `fk_tarifs_articles1_idx` (`articles_idarticles`),
+  ADD KEY `fk_tarifs_time1_idx` (`time_idtime`);
+
+--
+-- Index pour la table `time`
+--
+ALTER TABLE `time`
+  ADD PRIMARY KEY (`idtime`);
+
+--
+-- Index pour la table `types_utilisateur`
+--
+ALTER TABLE `types_utilisateur`
+  ADD PRIMARY KEY (`iduser_type`),
+  ADD UNIQUE KEY `iduser_type_UNIQUE` (`iduser_type`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id_utilisateur`),
+  ADD UNIQUE KEY `idusers_UNIQUE` (`id_utilisateur`),
+  ADD KEY `fk_utilisateurs_adresses_utilisateurs_idx` (`adresses_utilisateurs_id_adresses_utilisateur`),
+  ADD KEY `fk_utilisateurs_sexe1_idx` (`sexe_id_sexe`),
+  ADD KEY `fk_utilisateurs_types_utilisateur1_idx` (`types_utilisateur_iduser_type`),
+  ADD KEY `fk_utilisateurs_CB1_idx` (`CB_idCB`);
+
+--
+-- Index pour la table `villes_utilisateur`
+--
+ALTER TABLE `villes_utilisateur`
+  ADD PRIMARY KEY (`id_ville`),
+  ADD UNIQUE KEY `idville_UNIQUE` (`id_ville`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `adresses_utilisateurs`
+--
+ALTER TABLE `adresses_utilisateurs`
+  MODIFY `id_adresses_utilisateur` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `idarticles` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT pour la table `bornes_ville`
+--
+ALTER TABLE `bornes_ville`
+  MODIFY `id_bornes_ville` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `paniers`
+--
+ALTER TABLE `paniers`
+  MODIFY `id_paniers` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `sexe`
+--
+ALTER TABLE `sexe`
+  MODIFY `id_sexe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `types_utilisateur`
+--
+ALTER TABLE `types_utilisateur`
+  MODIFY `iduser_type` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `villes_utilisateur`
+--
+ALTER TABLE `villes_utilisateur`
+  MODIFY `id_ville` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Contraintes pour les tables déchargées
@@ -325,8 +441,3 @@ ALTER TABLE `utilisateurs`
   ADD CONSTRAINT `fk_utilisateurs_adresses_utilisateurs` FOREIGN KEY (`adresses_utilisateurs_id_adresses_utilisateur`) REFERENCES `adresses_utilisateurs` (`id_adresses_utilisateur`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_utilisateurs_sexe1` FOREIGN KEY (`sexe_id_sexe`) REFERENCES `sexe` (`id_sexe`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_utilisateurs_types_utilisateur1` FOREIGN KEY (`types_utilisateur_iduser_type`) REFERENCES `types_utilisateur` (`iduser_type`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
